e7f34e8af6e8b28bbc791f9b970b1034e8d59edc
Alexandre <alexandrehrtm@protonmail.com>
2020-03-28 21:24:06 -0300

    Validação adicionada

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
@@ -13,16 +14,52 @@ routes.post('/sessions', SessionController.create);
 // Listar todas as ongs
 routes.get('/ongs', OngController.index);
 // Criar ong
-routes.post('/ongs', OngController.create);
+routes.post(
+  '/ongs',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required().min(10).max(11),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2),
+    }),
+  }),
+  OngController.create
+);
 
 // Listar todos os casos
-routes.get('/incidents', IncidentController.index);
+routes.get(
+  '/incidents',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+    }),
+  }),
+  IncidentController.index
+);
 // Criar novo caso
 routes.post('/incidents', IncidentController.create);
 // Deletar um caso
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete(
+  '/incidents/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  IncidentController.delete
+);
 
 // Listar casos de uma ONG
-routes.get('/profile', ProfileController.index);
+routes.get(
+  '/profile',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  ProfileController.index
+);
 
 module.exports = routes;
